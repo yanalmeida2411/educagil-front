2839900edbf3a55ef88ff582ac167d6b82d7662a
Yan Monteiro <yanalmeida2411@gmail.com>
2026-09-22 14:39:37 -0300

    create frontend components

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -13,5 +13,6 @@
 export type ClassValue = string | number | bigint | boolean | null | undefined;
 
 export function cn(...values: ClassValue[]): string {
+  
   return values.filter((value): value is string => typeof value === 'string' && value !== '').join(' ');
 }
